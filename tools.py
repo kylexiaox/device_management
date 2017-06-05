29773ae6aca54ef8a5066e07509b11aec4439bd4
xiangxiao <xiaoxiang@cmvideo.cn>
2017-06-05 17:10:46 +0800

    update

--- tools.py
+++ tools.py
@@ -8,6 +8,6 @@ def send_email(recipients):
     subject = u"感谢您申领测试机,请填写附件表一并签字,交至市场部肖翔处"
     msg = Message(subject, sender=(u"肖翔","xiaoxiang@migu.cn"),recipients=recipients)
     msg.body = u"感谢您申领测试机,请填写附件表一并签字,交至市场部肖翔处"
-    with app.open_resource(u"测试终端管理办法.pdf") as fp:
+    with app.open_resource(u"regulations.pdf") as fp:
         msg.attach(filename=u"regulations.pdf", content_type="application/pdf",data= fp.read(),disposition='Content-Disposition')
     mail.send(msg)
